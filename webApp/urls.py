29f053375e845d316a6ffb47a8f05f390dc42bb2
dabriles@uniminuto.edu.co <dabriles@uniminuto.edu.co>
2018-11-27 20:20:56 -0500

    Cargue de html

--- webApp/urls.py
+++ webApp/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [url(r'^$', views.index, name='index'),
+urlpatterns = [url(r'^$', views.inicio, name='inicio'),
+	      
 	#url(r'^registar_terapeuta/$', views.registerTherapist, name='register_therapist'),
 	#url(r'^consultar_paciente/$', views.searchPatient, name='search_patient'),
 ]
--- webApp/views.py
+++ webApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def inicio(request):
 	return render(request, 'webApp/index.html')
+
 
