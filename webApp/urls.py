806f5b3be529ffd6c95076837462734fa884e0d4
dabriles@uniminuto.edu.co <dabriles@uniminuto.edu.co>
2018-11-28 22:25:57 -0500

    index completo

--- webApp/urls.py
+++ webApp/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [url(r'^$', views.inicio, name='inicio'),
-              url(r'^datos/$', views.datos, name='datos'),
+              url(r'^liga/$', views.liga, name='liga'),
+	      url(r'^resultados/$', views.resultados, name='resultados'),
+	      url(r'^apuestas/$', views.apuestas, name='apuestas'),
 	      url(r'^contacto/$', views.contacto, name='contacto'),
-	      url(r'^elementos/$', views.elementos, name='elementos'),
 	      url(r'^servicio/$', views.servicio, name='servicio'),
-              url(r'^detalle/$', views.detalle, name='detalle'),
+
 	      
 	#url(r'^registar_terapeuta/$', views.registerTherapist, name='register_therapist'),
 	#url(r'^consultar_paciente/$', views.searchPatient, name='search_patient'),
--- webApp/views.py
+++ webApp/views.py
@@ -4,19 +4,19 @@ from django.shortcuts import render
 def inicio(request):
 	return render(request, 'webApp/index.html')
 
-def datos(request):
+def liga(request):
+	return render(request, 'webApp/about.html')
+
+def resultados(request):
+	return render(request, 'webApp/services.html')
+
+def apuestas(request):
 	return render(request, 'webApp/blog.html')
 
 def contacto(request):
 	return render(request, 'webApp/contact.html')
 
-def elementos(request):
-	return render(request, 'webApp/elements.html')
-
 def servicio(request):
 	return render(request, 'webApp/services.html')
-
-def detalle(request):
-	return render(request, 'webApp/about.html')
 
 
