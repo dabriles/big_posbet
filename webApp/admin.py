f38d7829759f4d9e1a07c41fcc0fd086ffed3c6a
dabriles@uniminuto.edu.co <dabriles@uniminuto.edu.co>
2018-11-29 21:50:29 -0500

    Datos1

--- webApp/admin.py
+++ webApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import equipo
+from .models import goles
+from .models import equipo_ciudad
+
 
 # Register your models here.
+
+admin.site.register(equipo)
+admin.site.register(goles)
+admin.site.register(equipo_ciudad)
+
+
--- webApp/models.py
+++ webApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+	
+class equipo (models.Model):
+	EquipoLocal = models.TextField(max_length=30)
+	EquipoVisitante = models.TextField(max_length=30)
+	
+
+class goles (models.Model):
+	golesLocal = models.TextField(max_length=10)
+	golesVisitante = models.TextField(max_length=10)
+
+
+class equipo_ciudad (models.Model):
+	Equipo = models.TextField(max_length=10)
+	Ciudad = models.TextField(max_length=10)
+	
