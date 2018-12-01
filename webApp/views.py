9c8668f315fd0f698ee5f019f159dae54d67378b
dabriles@uniminuto.edu.co <dabriles@uniminuto.edu.co>
2018-12-01 16:42:37 -0500

    graficas

--- webApp/views.py
+++ webApp/views.py
@@ -2,6 +2,24 @@ from django.shortcuts import render
 
 from bokeh.plotting import figure
 from bokeh.embed import components
+import pandas as pd
+
+#datos1=pd.read_csv('/home/dsabrile/proyecto_dip/ciudad_equipos.csv',header=0)
+#print (datos1['Ciudad'])
+
+#def grafica1(request);
+#	datos=pd.read.csv("/home/dsabrile/proyecto_dip")
+
+
+
+
+
+
+
+
+
+
+#
 
 
 # Create your views here.
@@ -9,7 +27,30 @@ def inicio(request):
 	return render(request, 'webApp/index.html')
 
 def liga(request):
-	return render(request, 'webApp/about.html')
+	#return render(request, 'webApp/about.html')
+
+	x = [i for i in range(2016,2020)]
+	y = [6, 10, 17, 21]
+
+	title_plot_1 = ""
+	x_axis_1 = "Años"
+	y_axis_1 = "Goles"
+	plot_1_width = 500
+	plot_1_height = 350
+
+	plot_1 = figure(title= title_plot_1, x_axis_label= x_axis_1, y_axis_label= y_axis_1, plot_width= plot_1_width, plot_height= plot_1_height)
+
+	plot_1.circle(x, y)
+	plot_1.line(x, y, legend= '', line_width =2)
+
+	script_1, div_1 =components(plot_1)
+
+	return render(request, 'webApp/services.html', {'script_plot_1': script_1, 'div_plot_1' : div_1}) 
+
+
+
+
+
 
 def resultados(request):
 	
